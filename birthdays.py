c95fcca7c2f16638adf5966df57ec4b36d3babb0
rainapendurkar1234 <rainapendurkar@gmail.com>
2026-04-08 13:07:13 +0530

    birthdays project

---
+++ birthdays.py
@@ -0,0 +1,12 @@
+ 
+birthday_1 = ("Feburary 4")
+birthday_2 = ("March 29")
+birthday_3 = ("January 15")
+birthday_4 = ("September 28")
+birthday_5 = ("April 24")
+
+print(birthday_1)
+print(birthday_2)
+print(birthday_3)
+print(birthday_4)
+print(birthday_5)
