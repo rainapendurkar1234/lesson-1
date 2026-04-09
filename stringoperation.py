597ba096e129af492f3ec4d2e5951ab3e878fb22
rainapendurkar1234 <rainapendurkar@gmail.com>
2026-04-09 17:22:10 +0530

    project

---
+++ stringoperation.py
@@ -0,0 +1,22 @@
+
+# repeitions 
+a = "Raina "
+print(a*5)
+
+# concatenation
+c = "Raina"
+b = "Pendurkar"
+print(a + " " +b)
+
+# length of string
+str = "Watermelon"
+print(len(str))
+
+#Indexing
+string = "characteristics"
+print(string[8])
+
+# slicing
+strin = "ecosystem"
+print(strin[0:3])
+print(strin[::-1])
